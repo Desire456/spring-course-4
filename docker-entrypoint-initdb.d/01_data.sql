688658fde823248234266a29db73a4daf248d291
Vladislav Selitskii <selitskiivd@haulmont.com>
2021-08-04 18:28:53 +0400

    Fixes #3

---
+++ docker-entrypoint-initdb.d/01_data.sql
@@ -0,0 +1,5 @@
+insert into users values (1, 'username1');
+insert into users values (2, 'username2');
+insert into users values (3, 'username3');
+insert into users values (4, 'username4');
+insert into users values (5, 'username5');
